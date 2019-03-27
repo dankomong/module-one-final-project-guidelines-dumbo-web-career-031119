50865fb8a777d7aaf21edb0196c864a75ba3bfaa
beschwa <naraiswar@gmail.com>
2019-03-27 17:07:03 -0400

    restructure

--- bin/run.rb
+++ bin/run.rb
@@ -34,104 +34,96 @@ def welcome_menu
       elsif main_menu_choice == 'Log-in'
           sign_in(username, pass)
       end
-    end
+  end
+  return 1
 end
 
 def timetoduel
-  system "clear"
-  welcome_menu
-  system "clear"
-  deck_menu_1
-end
-
-def deck_menu_1
-  puts "Welcome #{$current_user.name}!"
-  deckloop = true
-  while(deckloop)
-    deck_menu_choice = $prompt.select("What would you like to do?", ["View Decks", "Create Deck"])
-    if deck_menu_choice == "View Decks"
-      if $current_user.decks == []
-        puts "You don't have any decks. Please create one."
-      else
-        $current_deck = $prompt.select("Deck List") do |menu|
-          $current_user.decks.each do |deck|
-            menu.choice deck.name, deck
-          end
-        end
-        # separate method here to customize the deck
-        customize_deck
-      end
-    elsif deck_menu_choice == "Create Deck"
-      create_deck
+  prompt_selection = [0]
+  while prompt_selection[0] != nil
+    #system "clear"
+    case prompt_selection[0]
+      when 0
+        prompt_selection[0] = welcome_menu
+      when 1
+        prompt_selection[0] = deck_choice_menu
+      when 2
+        prompt_selection[0] = view_decks
+      when 3
+        prompt_selection[0] = create_deck
+      when 4
+        prompt_selection[0] = deck_menu
+      when 5
+        prompt_selection[0] = deck_card_list
     end
   end
+end
+
+
+def deck_choice_menu
+  puts "Welcome #{$current_user.name}!"
+  deck_menu_arr = [{name: "View Decks", value: 2}, {name: "Create Deck", value: 3}]
+  $current_user.decks == [] ? (deck_menu_arr[0] = {name: "View Decks".colorize(:red), disabled: "(you don't have any decks to view)"}) : ""
+  $prompt.select("What would you like to do?", deck_menu_arr)
+end
+
+
+def view_decks
+  $current_deck = $prompt.select("#{$current_user.name}'s deck list") do |menu|
+    $current_user.decks.each do |deck|
+    menu.choice deck.name, deck
+    end
+  end
+  return 4
 end
 
 def create_deck
   deck_name = $prompt.ask('What would you like to name your deck?', required: true)
   $current_user.decks.create(name: deck_name)
+  return 2
 end
 
 
-def card_info(selected_card)
-  if selected_card.level != nil
-    puts "STATS"
-    puts "Name: #{selected_card.name}"
-    puts "Type: #{selected_card.cardtype}"
-    puts "Race: #{selected_card.race}"
-    puts "Attack: #{selected_card.attack}"
-    puts "Defense: #{selected_card.defense}"
-    puts "Attribute: #{selected_card.cardattr}"
-    puts "Description: #{selected_card.description}"
-  else
-    puts "STATS"
-    puts "Name: #{selected_card.name}"
-    puts "Type: #{selected_card.cardtype}"
-    puts "Race: #{selected_card.race}"
-    puts "Description: #{selected_card.description}"
-  end
-end
-
-
-def current_card_list
-  selected_cards = $prompt.select("Card List") do |menu|
+def deck_card_list
+  selected_card = $prompt.select("Card List") do |menu|
     $current_deck.cards.each_with_index do |card, index|
       menu.choice "#{index + 1}. #{card.name}", card
     end
   end
-  card_info(selected_cards)
+  selected_card.print_card_info
+  return 5
+end
+
+def deck_menu
+  deck_menu_arr = [{name: "View Cards", value: 5}, {name: "Customize Deck", value: 6}, {name: "Back", value: 2}]
+  $current_deck.cards.length == 0 ? (deck_menu_arr[0] = {name: "View Cards".colorize(:red), disabled: "(you don't have any cards in this deck"}) : ""
+  $prompt.select("Customizing your Deck: #{$current_deck.name}", deck_menu_arr)
+  # if card_menu_choice == "View Cards"
+  #   current_card_list
+  # elsif card_menu_choice == "Customize Deck"
+  #   # bring them to customize deck menu
+  #   card_menu
+  # end
 end
 
 
-def deck_menu_2
-  counter = 1
-  card_menu_choice = $prompt.select("Customize your cards!", ["View Cards", "Customize Deck", "Back"])
-  if card_menu_choice == "View Cards"
-    current_card_list
-  elsif card_menu_choice == "Customize Deck"
-    # bring them to customize deck menu
-    card_menu
-  end
-end
+# def customize_deck
+#   if $current_deck.cards.length == 0
+#     puts "You have no cards. Please add a card first."
+#     menu_choice = $prompt.select("Please select a choice:", ["Go to Cards Menu", "Back"])
+#     if menu_choice == "Go to Cards Menu"
+#       # bring them to the card menu through card_menu method
+#       card_menu
+#     else
+#       # back method
+#     end
+#   else
+#     deck_menu_2
+#   end
+# end
 
 
-def customize_deck
-  if $current_deck.cards.length == 0
-    puts "You have no cards. Please add a card first."
-    menu_choice = $prompt.select("Please select a choice:", ["Go to Cards Menu", "Back"])
-    if menu_choice == "Go to Cards Menu"
-      # bring them to the card menu through card_menu method
-      card_menu
-    else
-      # back method
-    end
-  else
-    deck_menu_2
-  end
-end
-
-
-def card_menu
+def card_choice_menu
     card_menu_choice = $prompt.select("Welcome to the cards menu!", ["View a Card", "Add a card", "Back"])
     if card_menu_choice == "View a Card"
       filter_cards_by_search
@@ -162,7 +154,7 @@ def filter_cards_by_search
     puts "Sorry, there are no cards that match your search."
   else
     cards_arr.each do |card|
-      card_info(card)
+      card.print_card_info
     end
   end
 #user_input = $prompt.ask("Type in the #{card_stat_choice} of the card")
@@ -183,3 +175,53 @@ end
 timetoduel
 
 #################### Program Ends ####################
+
+# def card_info(selected_card)
+#   if selected_card.level != nil
+#     puts "STATS"
+#     puts "Name: #{selected_card.name}"
+#     puts "Type: #{selected_card.cardtype}"
+#     puts "Race: #{selected_card.race}"
+#     puts "Attack: #{selected_card.attack}"
+#     puts "Defense: #{selected_card.defense}"
+#     puts "Attribute: #{selected_card.cardattr}"
+#     puts "Description: #{selected_card.description}"
+#   else
+#     puts "STATS"
+#     puts "Name: #{selected_card.name}"
+#     puts "Type: #{selected_card.cardtype}"
+#     puts "Race: #{selected_card.race}"
+#     puts "Description: #{selected_card.description}"
+#   end
+# end
+#
+# def current_card_list
+#   selected_cards = $prompt.select("Card List") do |menu|
+#     $current_deck.cards.each_with_index do |card, index|
+#       menu.choice "#{index + 1}. #{card.name}", card
+#     end
+#   end
+#   card_info(selected_cards)
+# end
+# def deck_menu_1
+#   puts "Welcome #{$current_user.name}!"
+#   deckloop = true
+#   while(deckloop)
+#     deck_menu_choice = $prompt.select("What would you like to do?", ["View Decks", "Create Deck"])
+#     if deck_menu_choice == "View Decks"
+#       if $current_user.decks == []
+#         puts "You don't have any decks. Please create one."
+#       else
+#         $current_deck = $prompt.select("Deck List") do |menu|
+#           $current_user.decks.each do |deck|
+#             menu.choice deck.name, deck
+#           end
+#         end
+#         # separate method here to customize the deck
+#         customize_deck
+#       end
+#     elsif deck_menu_choice == "Create Deck"
+#       create_deck
+#     end
+#   end
+# end
